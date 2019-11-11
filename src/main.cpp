d9486316bc793801f5a0dcbcb84816a8a4c55394
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2019-11-11 18:59:49 +0100

    Modify code

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,15 @@ TMC2130Stepper motor_config(14, 21, 20, 10, 11, 12, 13);
 Stepper motor(20, 21);       // STEP pin: 2, DIR pin: 3
 StepControl controller;    // Use default settings
 
-uint32_t step = 48*16, max_speed = step*10;
+uint32_t step = 48, max_speed = step;
 int32_t acceleration = max_speed;
 
 void setup() {
     // Configure the driver
     motor_config.begin(); // Initiate pins and registeries
 	motor_config.SilentStepStick2130(600); // Set stepper current to 600mA
-	motor_config.microsteps(16); // Set microstep to 16
+	motor_config.microsteps(0); // Set microstep to 16
+    // motor_config.stealthChop(true);
 
     // Configure the speed profile
     motor.setMaxSpeed(max_speed);
@@ -22,10 +23,11 @@ void setup() {
     // Enable the driver
 	digitalWrite(14, LOW);
 
+    motor.setTargetRel(step);  // Set target position to 1000 steps from current position
+    controller.move(motor);    // Do the move
+
 }
 
 void loop() {
-  motor.setTargetRel(1000);  // Set target position to 1000 steps from current position
-  controller.move(motor);    // Do the move
-  delay(500);
+
 }
